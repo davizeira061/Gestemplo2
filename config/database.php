38adafb5f68e243273f814ebaf1ddea640e50f0b
Davi Damasceno Cavalcante <venum.davi@gmail.com>
2025-08-07 17:16:59 -0300

    update

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'church_cms');
+define('DB_NAME', 'igreja');
 
 // --- Base URL Configuration ---
 // Dynamically determine the base URL
