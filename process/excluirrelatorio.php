3b7c2ca69fcb559d5b1ddec9757c459eb9f51c3f
Davi Damasceno Cavalcante <venum.davi@gmail.com>
2025-08-07 15:15:36 -0300

    exclusão

--- process/excluirrelatorio.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "../components/acessorestrito.php";
-$id = $_GET['id'];
-include_once "../components/conexao.php";
-$comando = "delete from relatorio where idRelatorio = $id";
-mysqli_query($conn, $comando);
-
-header("location: ../relatorios.php?msg=Relatório Excluido com Sucesso!");
-?>
--- process/sair.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "../components/acessorestrito.php";
-include_once '../components/conexao.php';
-$id = $_SESSION['idLider'];
-$logado = "update lider set logado = 1 where idLider = $id";
-mysqli_query($conn, $logado);
-session_destroy();
-header("location: ../index.php")
-?>
